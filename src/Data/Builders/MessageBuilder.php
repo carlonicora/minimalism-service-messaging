6b9ba11e9ae17c01b622cf707036caaf5bf63aec
Sergey Kuzminich <SergeyKuzminich@yandex.ru>
2021-04-08 11:52:40 +0300

    OpenAPI doc for message and thread

--- src/Data/Builders/MessageBuilder.php
+++ src/Data/Builders/MessageBuilder.php
@@ -8,6 +8,52 @@ use CarloNicora\Minimalism\Services\Builder\Objects\RelationshipBuilder;
 use CarloNicora\Minimalism\Services\Messaging\Data\DataReaders\UsersDataReader;
 use Exception;
 
+/**
+ * @OA\Schema(
+ *     schema="messageIdentifier",
+ *     description="Message identifier",
+ *     title="Message resource identifier",
+ *     @OA\Property(property="id", type="string", nullable=false, minLength=18, maxLength=18, example="p6Da23dZNybX4Wz95K"),
+ *     @OA\Property(property="type", type="string", nullable=false, example="message")
+ * )
+ *
+ * @OA\Schema(
+ *     schema="message",
+ *     description="Message resource",
+ *     title="Message",
+ *     allOf={@OA\Schema(ref="#/components/schemas/messageIdentifier")},
+ *     @OA\Property(property="attributes", ref="#/components/schemas/messageAttributes"),
+ *     @OA\Property(property="links", ref="#/components/schemas/messageLinks"),
+ *     @OA\Property(property="relationships", ref="#/components/schemas/messageRelationships"),
+ *     @OA\Property(property="meta", ref="#/components/schemas/messageMeta")
+ * )
+ *
+ * @OA\Schema(
+ *     schema="messageAttributes",
+ *     description="Message resource attributes",
+ *     title="Message attributes",
+ *     @OA\Property(property="creationTime", type="string", format="date-time", nullable=false, example="2021-01-01 23:59:59"),
+ *     @OA\Property(property="content", type="string", format="", nullable=false, minLength="1", maxLength="100", example="A content"),
+ *     @OA\Property(property="isUnread", type="boolean", example=true)
+ * )
+ *
+ * @OA\Schema(
+ *     schema="messageLinks",
+ *     description="Message links",
+ *     title="Message resource links",
+ *     @OA\Property(property="self", type="string", format="uri", nullable=false, minLength="1", maxLength="100", example="https://api.phlow.com/v2.1/threads/pOGm53wqVeZaB4dDyX/messages/p6Da23dZNybX4Wz95K")
+ * )
+ *
+ * @OA\Schema(
+ *     schema="messageRelationships",
+ *     description="Message relationships",
+ *     title="Message resource relationships",
+ *     @OA\Property(property="sender", ref="#/components/schemas/userIdentifier")
+ * )
+ *
+ * Class MessageBuilder
+ * @package CarloNicora\Minimalism\Services\Messaging\Data\Builders
+ */
 class MessageBuilder extends AbstractResourceBuilder
 {
     /** @var string  */
